c5fc055ab98247ea7512ba18d3454209116038bb
foviedo232 <141380909+foviedo232@users.noreply.github.com>
2024-01-03 14:15:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let contadorRef = firebase.database().ref('contador');
 let contador = 0;
 const contadorNumero = document.getElementById('contadorNumero');
 
-// Obtiene el valor inicial del contador desde la base de datos
+// Obtén el valor inicial del contador desde la base de datos
 contadorRef.once('value', (snapshot) => {
   contador = snapshot.val() || 0;
   actualizarContador();
@@ -10,17 +10,24 @@ contadorRef.once('value', (snapshot) => {
 
 function sumar() {
   contador++;
-  actualizarContador();
+  actualizarContadorEnFirebase(); // Actualiza el valor en la base de datos
 }
 
 function restar() {
   if (contador > 0) {
     contador--;
-    actualizarContador();
+    actualizarContadorEnFirebase(); // Actualiza el valor en la base de datos
   }
 }
 
 function actualizarContador() {
   contadorNumero.textContent = contador;
+}
+
+function actualizarContadorEnFirebase() {
+  actualizarContador();
   contadorRef.set(contador); // Actualiza el valor en la base de datos
 }
+
+// Actualiza el contador al cargar la página
+actualizarContador();
